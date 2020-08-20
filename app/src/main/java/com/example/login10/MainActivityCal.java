ffd1806122797789c71193782bdc08d761b70bf8
jw916203 <jw916203@gmail.com>
2020-08-21 02:22:16 +0900

    지우 지은 주영 / success 2020.08.20.

--- app/src/main/java/com/example/login10/MainActivityCal.java
+++ app/src/main/java/com/example/login10/MainActivityCal.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -53,10 +54,24 @@ public class MainActivityCal extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener callbackMethodDate;
     private TimePickerDialog.OnTimeSetListener callbackMethodTime;
 
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_cal);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView=(View)findViewById(R.id.drawer);
+
+        Button btn_open=(Button)findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
 
         myList=findViewById(R.id.MyList);
         // today=findViewById(R.id.today);
--- app/src/main/java/com/example/login10/UserActivity.java
+++ app/src/main/java/com/example/login10/UserActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
